a7fa8c92d35e7dd735d98555ef994c1312416d79
emmanuelhashy <emmanuelhashy@gmail.com>
2024-11-25 12:42:28 +0100

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { FileTreeDemo } from './pages/FileTreeDemo'
+
+function App() {
+
+  return (
+    <div className='flex justify-center mt-20'>
+    <FileTreeDemo/>
+    </div>
+  )
+}
+
+export default App
